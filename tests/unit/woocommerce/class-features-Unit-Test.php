3531ab808bed182af30ff3c5b52c0369519d72f6
Brian Henry <BrianHenryIE@gmail.com>
2023-09-06 20:25:04 -0700

    Ad coverage for `declare_cart_checkout_blocks_compatibility()`

--- tests/unit/woocommerce/class-features-Unit-Test.php
+++ tests/unit/woocommerce/class-features-Unit-Test.php
@@ -14,7 +14,7 @@ class Features_Unit_Test extends \Codeception\Test\Unit {
 	 * @covers ::__construct
 	 * @covers ::declare_hpos_compatibility
 	 */
-	public function test_no_features_class(): void {
+	public function test_no_hpos_compatibility(): void {
 
 		$settings = self::makeEmpty(
 			Settings_Interface::class,
@@ -26,5 +26,22 @@ class Features_Unit_Test extends \Codeception\Test\Unit {
 		$sut = new Features( $settings );
 
 		$sut->declare_hpos_compatibility();
+	}
+
+	/**
+	 * @covers ::__construct
+	 * @covers ::declare_cart_checkout_blocks_compatibility
+	 */
+	public function test_no_cart_checkout_blocks_compatibility(): void {
+		$settings = self::makeEmpty(
+			Settings_Interface::class,
+			array(
+				'get_plugin_basename' => Expected::never(),
+			)
+		);
+
+		$sut = new Features( $settings );
+
+		$sut->declare_cart_checkout_blocks_compatibility();
 	}
 }
--- tests/wpunit/woocommerce/class-features-WPUnit-Test.php
+++ tests/wpunit/woocommerce/class-features-WPUnit-Test.php
@@ -14,7 +14,7 @@ class Features_WPUnit_Test extends \Codeception\TestCase\WPTestCase {
 	/**
 	 * @covers ::declare_hpos_compatibility
 	 */
-	public function test_declare_compatibility(): void {
+	public function test_declare_hpos_compatibility(): void {
 
 		global $plugin_basename;
 
@@ -40,4 +40,32 @@ class Features_WPUnit_Test extends \Codeception\TestCase\WPTestCase {
 		self::assertContains( 'bh-wc-postcode-address-autofill/bh-wc-postcode-address-autofill.php', $result['compatible'], wp_json_encode( $result['compatible'] ) );
 	}
 
+	/**
+	 * @covers ::declare_cart_checkout_blocks_compatibility
+	 */
+	public function test_declare_cart_checkout_blocks_compatibility(): void {
+
+		global $plugin_basename;
+
+		$settings = self::makeEmpty(
+			Settings_Interface::class,
+			array(
+				'get_plugin_basename' => Expected::once( $plugin_basename ),
+			)
+		);
+
+		/**
+		 * `doing_action('before_woocommerce_init')` must be true.
+		 */
+		global $wp_current_filter;
+		$wp_current_filter[] = 'before_woocommerce_init';
+
+		$sut = new Features( $settings );
+
+		$sut->declare_cart_checkout_blocks_compatibility();
+
+		$result = FeaturesUtil::get_compatible_plugins_for_feature( 'cart_checkout_blocks' );
+
+		self::assertContains( 'bh-wc-postcode-address-autofill/bh-wc-postcode-address-autofill.php', $result['compatible'], wp_json_encode( $result['compatible'] ) );
+	}
 }
